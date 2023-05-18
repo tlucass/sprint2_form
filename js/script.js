07d2f91c8a38bc9bbdacf5e8cbae304efefb6d33
JLucazY <rm98458@fiap.com.br>
2023-05-18 16:36:44 -0300

    Capturando inputs no js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('myForm');
+
+const names = document.getElementById('name');
+const labelNames = document.querySelector("label[for='name']");
+
+const email = document.getElementById('email');
+const labelEmail = document.querySelector("label[for='email']");
+
+const password = document.querySelector("input[name='password']");
+const labelPassword = document.querySelector("label[for='password']");
+
+const phone = document.getElementById('phone');
+const labelPhone = document.querySelector("label[for='phone']");
+
+const age = document.getElementById('age');
+const labelAge = document.querySelector("label[for='age']");
+
+const gender = document.getElementById('gender');
